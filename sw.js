f3cfdc9ad0365f83d5ea7164254e7c96f7c7f5e9
Nanayaweggzy <150963030+Nanayaweggzy@users.noreply.github.com>
2026-02-17 12:07:03 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'easy-internet-v1';
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['/','/index.html'])));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
